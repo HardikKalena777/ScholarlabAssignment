bef6af03160c696fa3dacffabefb3d5c1f64eb30
Hardik <hardikkalena007@gmail.com>
2024-12-22 21:33:58 +0530

    Final Commit

--- Assets/Assignment1/Scripts/Draggable.cs
+++ Assets/Assignment1/Scripts/Draggable.cs
@@ -2,19 +2,73 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    Vector3 inputPositionOffset;
+    private Vector3 inputPositionOffset;
     public bool isDragging;
     public bool chemicalAdded;
     public bool resultAchieved;
-    Vector3 velocity;
-    Vector3 lastPosition;
+    private Vector3 velocity;
+    private Vector3 lastPosition;
     public float shakeThreshold;
     public float colorChangeSpeed;
     public Color targetColor;
     public Material conicalFlaskMaterial;
 
+    private static Draggable currentDraggedObject; // Tracks the currently dragged object
+
     private void Update()
     {
+        // Handle touch or mouse input
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = GetWorldPosition(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (IsTouchOverGameObject(touchPosition))
+                    {
+                        OnTouchBegin(touchPosition);
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (currentDraggedObject == this)
+                    {
+                        OnTouchMove(touchPosition);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (currentDraggedObject == this)
+                    {
+                        OnTouchEnd();
+                    }
+                    break;
+            }
+        }
+        else if (Input.GetMouseButton(0)) // Handle mouse input for testing in the editor
+        {
+            Vector3 mousePosition = GetWorldPosition(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsTouchOverGameObject(mousePosition))
+                {
+                    OnTouchBegin(mousePosition);
+                }
+            }
+            else if (Input.GetMouseButton(0) && currentDraggedObject == this)
+            {
+                OnTouchMove(mousePosition);
+            }
+            else if (Input.GetMouseButtonUp(0) && currentDraggedObject == this)
+            {
+                OnTouchEnd();
+            }
+        }
+
         if (isDragging)
         {
             // Calculate velocity
@@ -32,73 +86,48 @@ public class Draggable : MonoBehaviour
 
             lastPosition = transform.position;
         }
-
-        // Stop dragging on touch release or mouse button release
-        if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
-        {
-            isDragging = false;
-        }
     }
 
-    private Vector3 GetInputWorldPosition()
+    private void OnTouchBegin(Vector3 inputPosition)
     {
-        Vector3 inputScreenPosition;
-
-        if (Input.touchCount > 0) // Touch input
+        if (currentDraggedObject == null) // Only allow one object to be dragged
         {
-            inputScreenPosition = Input.GetTouch(0).position;
-        }
-        else // Mouse input
-        {
-            inputScreenPosition = Input.mousePosition;
-        }
-
-        inputScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z; // Maintain object's Z position
-        return Camera.main.ScreenToWorldPoint(inputScreenPosition);
-    }
-
-    private void OnMouseDown()
-    {
-        if (Input.touchCount == 0) // Only handle mouse when there are no touches
-        {
-            inputPositionOffset = gameObject.transform.position - GetInputWorldPosition();
-        }
-    }
-
-    private void OnMouseDrag()
-    {
-        if (Input.touchCount == 0) // Only handle mouse when there are no touches
-        {
-            transform.position = GetInputWorldPosition() + inputPositionOffset;
+            inputPositionOffset = transform.position - inputPosition;
             isDragging = true;
+            currentDraggedObject = this; // Set this object as the currently dragged object
         }
     }
 
-    private void OnMouseUp()
+    private void OnTouchMove(Vector3 inputPosition)
     {
-        isDragging = false;
-    }
-
-    private void OnTouchBegin()
-    {
-        if (Input.touchCount > 0)
+        if (isDragging)
         {
-            inputPositionOffset = gameObject.transform.position - GetInputWorldPosition();
-            isDragging = true;
-        }
-    }
-
-    private void OnTouchMove()
-    {
-        if (Input.touchCount > 0)
-        {
-            transform.position = GetInputWorldPosition() + inputPositionOffset;
-            isDragging = true;
+            transform.position = inputPosition + inputPositionOffset;
         }
     }
 
     private void OnTouchEnd()
     {
-        isDragging = false;
+        if (isDragging)
+        {
+            isDragging = false;
+            currentDraggedObject = null; // Clear the current dragged object
+        }
+    }
+
+    private Vector3 GetWorldPosition(Vector3 screenPosition)
+    {
+        screenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z; // Maintain Z position
+        return Camera.main.ScreenToWorldPoint(screenPosition);
+    }
+
+    private bool IsTouchOverGameObject(Vector3 worldPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Camera.main.WorldToScreenPoint(worldPosition));
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return hit.collider != null && hit.collider.gameObject == gameObject;
+        }
+        return false;
     }
 }
